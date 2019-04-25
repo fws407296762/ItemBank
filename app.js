b0724cba530f2c2e956df824da6a48a82c8d1a44
fws407296762 <fws407296762@qq.com>
2019-04-26 00:18:38 +0800

    创建数据库，并更新数据

--- app.js
+++ app.js
@@ -2,59 +2,28 @@ const express = require("express");
 const app = express();
 const https = require("https");
 const puppeteer = require("puppeteer");
+const DB = require("./db/config");
+let SqliteDB = new DB.SqliteDB("./db/database.db");
+;(async ()=>{
+  let url = "https://www.koolearn.com/shiti/list-1-3-104935-1.html"; //合成
+  let sujects = await getSubjects(url);
+  let sujectsAnwers = await getSubjectAnswer(sujects);
+  // console.log(sujectsAnwers)
+  let newSujectsAnwers = sujectsAnwers.map(item=>{
+    let options = item.options;
+    return [item.title,JSON.stringify(options),1,item.answer,true,"新东方"]
+  });
+  console.log(newSujectsAnwers);
 
-
-(async ()=>{
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://www.koolearn.com/shiti/list-1-3-104935-1.html");
-  let result = await page.evaluate(async ()=>{
-    let $parts = $(".i-tree>.content>.part");
-
-    let partData = [];
-    for(let i = 0;i<$parts.length;i++){
-      let item = $($parts[i]);
-      let $jiChapter = item.find(".ji-chapter");
-      let $partA = item.find(".title>a");
-      let part = {};
-      let title = $partA.text();
-      part.title = title;
-      part.href = "https://www.koolearn.com" + $partA.attr("href");
-      if($jiChapter.length > 0){
-        part.children = [];
-
-      }
-      partData.push(part);
-    }
-    return partData;
-  })
-  // let knowledgepoints = await page.$$eval(".i-tree>.content>.part",(nodes)=>{
-  //
-  //   let childrenList = nodes[0].children;
-  //   let partTree = {
-  //     title:"",
-  //     href:""
-  //   }
-  //   for(let i = 0;i<childrenList.length;i++){
-  //     let item = childrenList[i];
-  //     let itemClassName = item.className;
-  //     if(itemClassName === "title"){
-  //       partTree.title = item.innerText;
-  //       let itemChildren = item.children;
-  //
-  //     }
-  //   }
-  //   return window;
-  // })
-  console.log(result);
-  await browser.close();
+  SqliteDB.insertData("insert into tb_Subject(Subjectname,Subjectoption,Subjecttype,Subjectanswer,Subjectstatus,Subjectfrom) values(?,?,?,?,?,?)",newSujectsAnwers)
 })()
 
+
 //获取页面中题目
-async function getSubjects(){
+async function getSubjects(url){
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://www.koolearn.com/shiti/list-1-3-27589-1.html");
+  await page.goto(url);
   let $pResults = await page.$$eval(".i-timu>.content",async (nodes,abc)=>{
     let newresults = [];
     for(let i = 0;i<nodes.length;i++){
@@ -79,6 +48,7 @@ async function getSubjects(){
     return newresults
   });
   await browser.close();
+  return $pResults;
 }
 
 //获取题目中的答案
@@ -86,21 +56,25 @@ async function getSubjectAnswer(subjectList){
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   let index = 0;
-  (async function recursionGetAnswer(index) {
-    if(index === subjectList.length){
-      await browser.close();
-      console.log("$anResult:",subjectList);
-      return false;
-    }
-    console.log("index:",index);
-    await page.goto(subjectList[index].answerhref);
-    let $anResult = await page.$eval("#i-tab-content",async (node)=>{
-      return node.innerText
-    })
-    subjectList[index].answer = $anResult;
-    index++;
-    recursionGetAnswer(index);
-  })(index)
+  let newSubjectList = JSON.parse(JSON.stringify(subjectList));
+  return new Promise((resolve,reject)=>{
+    (async function recursionGetAnswer(index) {
+      if(index === newSubjectList.length){
+        await browser.close();
+        resolve(newSubjectList)
+        return false;
+      }
+      await page.goto(newSubjectList[index].answerhref);
+      let $anResult = await page.$eval("#i-tab-content",async (node)=>{
+        return node.innerText
+      })
+      newSubjectList[index].answer = $anResult;
+      index++;
+      recursionGetAnswer(index);
+    })(index);
+  })
+
+  console.log(newSubjectList)
 }
 
 // app.listen(8080)
