e1ff75ab1bb73e1b3a5f3da1ec4fbc407ca96fae
fuwensong <fws407296762@qq.com>
2020-01-20 18:00:17 +0800

    开发

--- crawler.js
+++ crawler.js
@@ -2,19 +2,21 @@ const express = require("express");
 const crawler = express();
 const https = require("https");
 const puppeteer = require("puppeteer");
+const koolearn = require("./config/koolearn");
 const DB = require("./db/config");
+
 let SqliteDB = new DB.SqliteDB("./db/database.db");
-;(async ()=>{
-  let url = "https://www.koolearn.com/shiti/list-1-3-27622-6.html";
-  let sujects = await getSubjects(url);
-  let sujectsAnwers = await getSubjectAnswer(sujects);
-  let newSujectsAnwers = sujectsAnwers.map(item=>{
-    let options = item.options;
-    return [item.title,JSON.stringify(options),12,item.answer,true,"新东方"]
-  });
-  // console.log(newSujectsAnwers);
-  SqliteDB.insertData("insert into tb_Subject(Subjectname,Subjectoption,Subjecttype,Subjectanswer,Subjectstatus,Subjectfrom) values(?,?,?,?,?,?)",newSujectsAnwers)
-})()
+// ;(async ()=>{
+//   let url = "https://www.koolearn.com/shiti/list-1-3-27622-6.html";
+//   let sujects = await getSubjects(url);
+//   let sujectsAnwers = await getSubjectAnswer(sujects);
+//   let newSujectsAnwers = sujectsAnwers.map(item=>{
+//     let options = item.options;
+//     return [item.title,JSON.stringify(options),12,item.answer,true,"新东方"]
+//   });
+//   // console.log(newSujectsAnwers);
+//   SqliteDB.insertData("insert into tb_Subject(Subjectname,Subjectoption,Subjecttype,Subjectanswer,Subjectstatus,Subjectfrom) values(?,?,?,?,?,?)",newSujectsAnwers)
+// })()
 
 
 //获取页面中题目
@@ -76,4 +78,33 @@ async function getSubjectAnswer(subjectList){
   })
 }
 
+//获取页面中知识点
+async function getClassification(){
+  console.log("开始执行...")
+  const brower = await puppeteer.launch();
+  const page = await brower.newPage();
+  //获取初中英语知识点
+  let pageRes = await page.goto(koolearn.route.juniorhigh);
+  let trees = await page.$eval(".i-left .i-card .i-tree div.content",el=>{
+    let trees = [];
+    let $jiParts = el.querySelectorAll(".ji-part");
+    for(let i = 0;i<$jiParts.length;i++){
+      let $title = $jiParts[i].querySelector(".title");
+      let $a = $title.querySelector("a");
+      let $jiChapter = $jiParts[i].querySelector(".ji-chapter");
+      (function getchapters(chapter){
+
+      })($jiChapter)
+      let $jiChapterPiece = $jiChapter.querySelectorAll(".ji-chapter-piece");
+      trees.push({
+        title:$a.textContent,
+        route:$a.href
+      })
+    }
+    return trees;
+  });
+
+  console.log("trees:",trees)
+}
+getClassification();
 // app.listen(8080)
